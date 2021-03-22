054023e816ba0334e1102c02a20fbef4ef40307b
Joseph Sipiorski <josephsipiorski@gmail.com>
2021-03-22 11:05:19 -0500

    update to ResizingIntSet

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -12,7 +12,9 @@ class MaxIntSet
   end
 
   def remove(num)
-
+    if is_valid?(num)
+      @store[num] = false
+    end
   end
 
   def include?(num)
@@ -42,21 +44,25 @@ class IntSet
   end
 
   def insert(num)
-
+    self[num] << num
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+    end
 
   end
 
   def include?(num)
-
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -88,6 +94,7 @@ class ResizingIntSet
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
