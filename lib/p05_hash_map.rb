636cf1b90c5264b08c1745a086814db95b49fa78
Joseph Sipiorski <josephsipiorski@gmail.com>
2021-03-22 15:24:25 -0500

    phase 5: Hash Map(reprise)

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -1,6 +1,7 @@
 require_relative 'p04_linked_list'
-
+require "byebug"
 class HashMap
+  include Enumerable
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,18 +10,39 @@ class HashMap
   end
 
   def include?(key)
+    self.each do |k, v|
+      return true if k == key
+    end
   end
 
   def set(key, val)
   end
 
   def get(key)
+    if include?(key)
+      self.each do |k,v|
+        return v if k == key 
+      end
+    else
+      return nil
+    end
   end
 
   def delete(key)
   end
 
-  def each
+  def each(&prc)
+  # debugger
+    i = 0
+    while i < num_buckets
+      @store[i].each do |node|
+        if node != self.head && node != self.tail
+          prc.call(node.key, node.value)
+        end
+      end
+      i += i
+    end
+
   end
 
   # uncomment when you have Enumerable included
